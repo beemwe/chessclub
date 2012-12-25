dca062f7c559a82111f1a568f259560ef2d91aea
Bernd M. Walter <info@walterdl.de>
2012-12-25 13:52:51 +0100

    - implementing changes for deploy on webserver

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,12 +4,37 @@ worker_processes 2
 preload_app true
 timeout 30
 listen root + 'tmp/sockets/unicorn.sock', :backlog => 64
+pid "/var/rails/tusffbschach/shared/tmp/pids/unicorn.personalconcept.pid"
+
+# Production specific settings
+if env == "production"
+  # Help ensure your application will always spawn in the symlinked
+  # "current" directory that Capistrano sets up.
+  working_directory "/var/rails/personalconcept/current"
+
+  # feel free to point this anywhere accessible on the filesystem
+  user 'schachclub', 'unicorn'
+  shared_path = "/var/rails/personalconcept/shared"
+
+  stderr_path "#{shared_path}/log/unicorn.stderr.log"
+  stdout_path "#{shared_path}/log/unicorn.stdout.log"
+end
+
+
 
 before_fork do |server, worker|
-  pid_old = root + 'tmp/pids/unicorn.pid.oldbin'
-  if File.exists?(pid_old) && server.pid != pid_old
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  # Before forking, kill the master process that belongs to the .oldbin PID.
+  # This enables 0 downtime deploys.
+  old_pid = "/var/rails/tusffbschach/shared/tmp/pids/unicorn.personalconcept.pid.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
     begin
-      Process.kill("QUIT", File.read(pid_old).to_i)
+      Process.kill("QUIT", File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
       # someone else did our job for us
     end
@@ -18,22 +43,4 @@ end
 
 after_fork do |server, worker|
   ActiveRecord::Base.establish_connection
-  begin
-    uid, gid = Process.euid, Process.egid
-    user, group = 'beemwe', 'beemwe'
-    target_uid = Etc.getpwnam(user).uid
-    target_gid = Etc.getgrnam(group).gid
-    worker.tmp.chown(target_uid, target_gid)
-    if uid != target_uid || gid != target_gid
-      Process.initgroups(user, target_gid)
-      Process::GID.change_privilege(target_gid)
-      Process::UID.change_privilege(target_uid)
-    end
-  rescue => e
-    if env == 'development'
-      STDERR.puts "Cannot change Unicorn's worker UID/GID in development environment."
-    else
-      raise e
-    end
-  end
 end
