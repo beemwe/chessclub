5a54ae0035af4c80ee5aad8794c26a6214795e28
Bernd M. Walter <bernd.m.walter@gmail.com>
2013-05-26 14:30:53 +0200

    fixing error in Torunament::make_table_array

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -52,7 +52,7 @@ class Tournament < ActiveRecord::Base
   end
 
   def make_table_array
-    buffer = self.tournament_players.map{|p| [p.id, "#{p.first_name} #{p.last_name}", p.result[:points], p.result[:place]]}.sort_by{|p| -p[2]}
+    buffer = self.tournament_players.map{|p| [p.id, "#{p.first_name} #{p.last_name}", p.result[:points], p.result[:place].blank? ? 1 : p.result[:place]]}.sort_by{|p| -p[2]}
     points = buffer[0][2]
     place = 1
     buffer.each do |p|
