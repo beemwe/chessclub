1855156e56c0d180f0138ed80d87c7f5c39aa7a8
Bernd M. Walter <bernd.m.walter@gmail.com>
2013-10-31 20:46:39 +0100

    implementing the new import for DEWIS data (clubs and organization_players)

--- app/models/organization_player.rb
+++ app/models/organization_player.rb
@@ -1,3 +1,4 @@
+# encoding:utf0
 class OrganizationPlayer < ActiveRecord::Base
   # set_primary_key :index
   attr_accessible :birth_year, :club, :club_id, :dewis_club_id, :dwz, :elo, :fide_title, :first_name, :last_name, :pkz,
@@ -12,19 +13,21 @@ class OrganizationPlayer < ActiveRecord::Base
   def self.generate_from_dewis(csv)
     # 0: ID, 1:VKZ, 2:Mgl-Nr, 3:Status, 4:Spielername, 6:Geschlecht, 7:Spielberechtigung, 8:Geburtsjahr, 9:Letzte-Auswertung,
     # 10:DWZ, 11: Index, 12:FIDE-Elo, 13:FIDE-Titel, 14:FIDE-ID, 15:FIDE-Land
-    player = nil
+    result = ''
     data = csv.gsub(/"/, '').split(',')
     pkz = data[0].to_i
     if pkz.to_s == data[0]
       player = OrganizationPlayer.find_or_initialize_by_pkz pkz
+      result += player.new_record? ? 'Neu     : ' : 'Geändert: '
       player.update_attributes dewis_club_id: data[1].to_i, status: data[3], last_name: data[4], first_name: data[5],
                                gender: data[6], birth_year: data[8].to_i, dwz: data[10].to_i, elo: data[12].to_i,
                                fide_title: data[13]
+      result += data[4] + ', ' + data[5]
     else
-      false
+      result = 'PKZ ist keine Integerzahl!'
     end
 
-    player
+    result
   end
 
   private
--- lib/tasks/dewis.rake
+++ lib/tasks/dewis.rake
@@ -15,14 +15,13 @@ namespace :dewis do
     end
   end
 
+  desc 'Updates all Players of Deutscher Schachbund via CSV file'
   task :update_players, [:file_name] => [:environment] do |t, args|
     file_name = args[:file_name]
     puts "Lese Datei: #{file_name}"
     CSV.foreach(file_name, :headers => true) do |row|
       result = OrganizationPlayer.generate_from_dewis row.to_s
-      if result.class.to_s == 'OrganizationPlayer'
-        puts "#{result.first_name} #{result.last_name} updated."
-      end
+      puts result
     end
   end
 
